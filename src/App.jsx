a30d46325ddfc3923d29e23856a37a1d884ce3ca
sumitverma789987-lang <sumitverma789987@gmail.com>
2026-01-22 11:58:22 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
 import bgImage from "../assets/bg-image.png";
-import Text from "./components/text";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+import Text from "./components/Text";
+
 
 
 function App() {
   return (
     <div
-      className=" flex h-screen bg-cover bg-center z-20"
+      className=" flex h-screen bg-cover bg-center z-20 "
       style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="absolute inset-0 bg-black/50" />
 
       <Navbar />
-      <div className=" flex  ">
+      <div className=" flex absolute  ">
 
         <Text />
       </div>
